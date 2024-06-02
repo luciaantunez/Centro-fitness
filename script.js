278c091169d4ed4329c8c4b637949c896f54f0e7
Lucía Antúnez <lucia075a@gmail.com>
2024-06-02 18:09:10 -0300

    cambios

--- script.js
+++ script.js
@@ -1,40 +1,25 @@
+const nav = document.getElementById("navbar");
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollPosition = window.scrollY;
+window.onscroll = function () {
+    var currentScrollPosition = window.scrollY;
+    if (prevScrollPosition > currentScrollPosition) {
+        nav.style.top = "0";
+    } else {
+        nav.style.top = "-50px";
+    }
+    prevScrollPosition = currentScrollPosition;
+}
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');
-    
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-
-        // Burger Animation
-        burger.classList.toggle('toggle');
-    });
-
-    document.getElementById('asesoria-online').addEventListener('click', function() {
-        document.getElementById('asesoria-o').scrollIntoView({ behavior: 'smooth' });
-    });
-    document.getElementById('inicio').addEventListener('click', function() {
-        document.getElementById('ini').scrollIntoView({ behavior: 'smooth' });
-    });
-    document.getElementById('logoGym').addEventListener('click', function() {
-        document.getElementById('ini').scrollIntoView({ behavior: 'smooth' });
-    });
-    document.getElementById('sobre-nosotros').addEventListener('click', function() {
-        document.getElementById('about-us').scrollIntoView({ behavior: 'smooth' });
-    });
-
-    });
-
-    
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  } */
