d05bf2ed861647c0836b3f9d26a7a96fcd01bd91
Lucía Antúnez <lucia075a@gmail.com>
2024-07-01 17:57:42 -0300

    Landing terminada HTML y CSS

--- script.js
+++ script.js
@@ -24,3 +24,50 @@ window.onscroll = function () {
     prevScrollPosition = currentScrollPosition;
 };
 
+
+
+/* FUNCIONes SECCIÓN TESTIMONIOS */
+let currentTestimonial = 0;
+const testimonials = document.querySelectorAll('.testimonial');
+const dots = document.querySelectorAll('.dot');
+
+// Mostrar el primer testimonio
+showTestimonial(currentTestimonial);
+
+document.querySelector('.fa-chevron-left').addEventListener('click', () => {
+    changeTestimonial(-1);
+});
+
+document.querySelector('.fa-chevron-right').addEventListener('click', () => {
+    changeTestimonial(1);
+});
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        showTestimonial(index);
+    });
+});
+
+function showTestimonial(index) {
+    testimonials.forEach((testimonial, i) => {
+        testimonial.style.display = i === index ? 'flex' : 'none';
+        dots[i].classList.toggle('active', i === index);
+    });
+}
+
+function changeTestimonial(direction) {
+    currentTestimonial = (currentTestimonial + direction + testimonials.length) % testimonials.length;
+    showTestimonial(currentTestimonial);
+}
+
+// Función para cambiar la imagen al hacer clic en pantallas pequeñas
+function toggleImage(container) {
+    const beforeImage = container.querySelector('.before');
+    const afterImage = container.querySelector('.after');
+    const isAfterVisible = afterImage.style.opacity == 1;
+
+    beforeImage.style.opacity = isAfterVisible ? 1 : 0;
+    afterImage.style.opacity = isAfterVisible ? 0 : 1;
+}
+
+    
